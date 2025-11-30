53b341a70507b29384bbe59ae63a29dd50d2271d
prab0779 <101067217+prab0779@users.noreply.github.com>
2025-11-30 20:58:35 +0000

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -96,7 +96,7 @@ export const Home: React.FC<HomeProps> = ({ items }) => {
       </section>
 
       {/* FAQ */}
-      <section className="relative z-10 max-w-7xl mx-auto px-4 md:px-6 py-12 md:py-16">
+      <section className="relative z-10 max-w-7xl mx-auto px-4 md:px-6 py-6 md:py-10">
         <FAQSection />
       </section>
 
