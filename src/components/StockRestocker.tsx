60a040833275d80762bd8d633494aca6502944dd
prab0779 <101067217+prab0779@users.noreply.github.com>
2026-04-20 15:16:21 +0100

    Updated StockRestocker.tsx

--- src/components/StockRestocker.tsx
+++ src/components/StockRestocker.tsx
@@ -105,7 +105,7 @@ export const StockRestocker: React.FC = () => {
             gradientTo="transparent"
           />
 
-          <GradientText variant="silver">>:</GradientText>
+          <GradientText colors={["#9a9a9a","#d1d5db","#ffffff","#d1d5db","#9a9a9a"]}>:</GradientText> 
 
           <Counter
             value={minutes}
