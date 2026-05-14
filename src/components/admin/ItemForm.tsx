455b4a33138feed7777a6257763da770e0de7bc6
prab0779 <101067217+prab0779@users.noreply.github.com>
2026-05-14 13:52:01 +0100

    Updated ItemForm.tsx

--- src/components/admin/ItemForm.tsx
+++ src/components/admin/ItemForm.tsx
@@ -119,7 +119,7 @@ export const ItemForm: React.FC<ItemFormProps> = ({
                   min="0"
                   step="any"
                   value={valueInput}
-                  onChange={(e) => parseFloat(e.target.value)}
+                  onChange={(e) => setValueInput(e.target.value)}
                   className={inputCls}
                   placeholder="0.00"
                 />
