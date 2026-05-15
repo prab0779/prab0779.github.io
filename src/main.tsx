a468efd91338ec8c6b814714b1f827aeec054698
prab0779 <101067217+prab0779@users.noreply.github.com>
2026-05-15 13:24:59 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { AuthProvider } from "./contexts/AuthContext";
 import { supabase } from "./lib/supabase";
 
 (function handleOAuthRedirect() {
@@ -32,7 +33,9 @@ import { supabase } from "./lib/supabase";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <AuthProvider>
       <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
