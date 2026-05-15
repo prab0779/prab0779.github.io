ce4961ae46513e34a6b7aca3dc4d98e41f28daf7
prab0779 <101067217+prab0779@users.noreply.github.com>
2026-05-15 13:30:54 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,37 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./contexts/AuthContext";
-import { supabase } from "./lib/supabase";
-
-(function handleOAuthRedirect() {
-  const hash = window.location.hash;
-
-  if (hash && hash.includes("access_token=")) {
-    const params = new URLSearchParams(hash.replace(/^#/, ""));
-    const accessToken = params.get("access_token");
-    const refreshToken = params.get("refresh_token") ?? "";
-
-    if (accessToken) {
-      supabase.auth.setSession({
-        access_token: accessToken,
-        refresh_token: refreshToken,
-      });
-
-      // remove oauth hash
-      window.history.replaceState(
-        null,
-        "",
-        window.location.pathname + window.location.search
-      );
-    }
-  }
-})();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-      <App />
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
